5274468fa5d333d8acee1c85676f4c41293232e0
luisfernando199 <luisfernando_91@hotmail.com>
2022-08-22 18:31:18 +0200

    Segunda version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyBQcYkSurTN3UfvicDtB9cZP53w6LS01T0",
+    authDomain: "personal-portfolio-luis.firebaseapp.com",
+    databaseURL: "https://personal-portfolio-luis-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "personal-portfolio-luis",
+    storageBucket: "personal-portfolio-luis.appspot.com",
+    messagingSenderId: "834680279806",
+    appId: "1:834680279806:web:46c1b2ffe1c2756abdcbaf",
+    measurementId: "G-5D9SMBYYJ8"
+  }
 };
+
+
